5a156e753e83efc6a8388a230dd368dcf96eb2b3
williamdmv <william_dan.mosquera@uao.edu.co>
2023-05-20 11:23:35 -0500

    timer v2

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class CountdownTimer : MonoBehaviour
 {
     public Text timerText;
+    public GameObject enemyObject;
     private float totalTime = 30f;
     private float currentTime;
+    private bool enemyActive = false;
 
     void Start()
     {
@@ -20,21 +22,32 @@ public class CountdownTimer : MonoBehaviour
         {
             currentTime -= Time.deltaTime;
             UpdateTimerText();
-        }
-        else
-        {
-            currentTime = 0f;
-            // Realizar acciones cuando el temporizador llega a cero
+
+            if (currentTime <= 0f && !enemyActive)
+            {
+                ActivateEnemy();
+            }
         }
     }
 
     void UpdateTimerText()
     {
         string seconds = ((int)currentTime % 60).ToString("00");
-        string milliseconds = ((currentTime * 1000) % 1000).ToString("000");
-
-        string timerString = string.Format("{0}:{1}", seconds, milliseconds);
+        string timerString = string.Format("{0}", seconds);
         timerText.text = timerString;
     }
 
+    void ActivateEnemy()
+    {
+        enemyObject.SetActive(true);
+        enemyActive = true;
+        // Realizar acciones adicionales cuando el temporizador llega a cero y el enemigo se activa
+
+        // Aquí puedes hacer referencia al enemigo por su tag
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemigo");
+        foreach (GameObject enemy in enemies)
+        {
+            // Realizar acciones específicas para cada enemigo
+        }
+    }
 }
