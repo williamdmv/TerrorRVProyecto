1a7ad847d027a09f7cd8b7c7ccce02e69391afa0
williamdmv <william_dan.mosquera@uao.edu.co>
2023-05-29 11:49:23 -0500

    X

--- Assets/colision.cs
+++ Assets/colision.cs
@@ -3,14 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class colision : MonoBehaviour
 {
-    public string sceneName = "Menu de inicio"; // Nombre de la escena a la que deseas cambiar
+    // Nombre de la escena a la que deseas cambiar
     
-        private void OnCollisionEnter(Collision collision)
+         void OnCollisionEnter(Collision other)
         {
-            if (collision.gameObject.CompareTag("Player"))
-            {
-                SceneManager.LoadScene(sceneName);
-            }
+            
+                SceneManager.LoadScene(0);
+            
         }
 
     
