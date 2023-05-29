38bb8c369c249c5dba600a92292376c2ef71bf47
williamdmv <william_dan.mosquera@uao.edu.co>
2023-05-29 17:34:38 -0500

    Corrigiendo marcas de conflicto en el scroipt colision en la rama main

--- Assets/colision.cs
+++ Assets/colision.cs
@@ -3,25 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class colision : MonoBehaviour
 {
-<<<<<<< HEAD
-    // Nombre de la escena a la que deseas cambiar
-=======
-    public string sceneName; // Nombre de la escena a la que deseas cambiar
->>>>>>> 667a9f230dbc946f61696ffc7d7b07fb0910752f
+    public string sceneName;
     
-         void OnCollisionEnter(Collision other)
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemigo"))
         {
-<<<<<<< HEAD
-            
-                SceneManager.LoadScene(0);
-            
-=======
-            if (collision.gameObject.CompareTag("Enemigo"))
-            {
-                SceneManager.LoadScene(sceneName);
-            }
->>>>>>> 667a9f230dbc946f61696ffc7d7b07fb0910752f
+            SceneManager.LoadScene(sceneName);
         }
+    }
 
     
 }
